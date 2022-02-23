0a59e447fc081163615ef959eeda0ee9a36b0a57
kelvinVargasUG <joel.vargas.ch@hotmail.com>
2022-02-22 21:08:37 -0500

    Proyecto Actualizado en la sala

--- src/main/java/UG/Proyecto/ProyectoUgApplication.java
+++ src/main/java/UG/Proyecto/ProyectoUgApplication.java
@@ -1,18 +1,26 @@
 package UG.Proyecto;
 
 import org.ocpsoft.rewrite.servlet.RewriteFilter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.SpringBootCondition;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 import javax.faces.webapp.FacesServlet;
 import javax.servlet.DispatcherType;
 import java.util.EnumSet;
+
+import com.sun.*;
+import javax.faces.webapp.FacesServlet;
+
 
 @EnableAutoConfiguration
 @ComponentScan({"com.auth0.samples.bootfaces"})
@@ -24,19 +32,31 @@ public class ProyectoUgApplication extends SpringBootServletInitializer {
     }
 
     @Bean
-    public ServletRegistrationBean servletRegistrationBean() {
-        FacesServlet servlet = new FacesServlet();
-        return new ServletRegistrationBean(servlet, "*.jsf");
+    public ServletRegistrationBean facesServletRegistration() {
+	ServletRegistrationBean registration = new ServletRegistrationBean<>(new FacesServlet(), "*.xhtml");
+	registration.setLoadOnStartup(1);
+//	registration.addUrlMappings("*.jr");
+	return registration;
     }
-    
+
     @Bean
-    public FilterRegistrationBean rewriteFilter() {
-        FilterRegistrationBean rwFilter = new FilterRegistrationBean(new RewriteFilter());
-        rwFilter.setDispatcherTypes(EnumSet.of(DispatcherType.FORWARD, DispatcherType.REQUEST,
-                DispatcherType.ASYNC, DispatcherType.ERROR));
-        rwFilter.addUrlPatterns("/*");
-        return rwFilter;
+    public ServletContextInitializer servletContextInitializer() {
+	return servletContext -> {
+	    servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+	    servletContext.setInitParameter("primefaces.THEME", "sunny");
+	};
     }
+
+    @Bean
+    public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
+	return new ServletListenerRegistrationBean<>(new ConfigureListener());
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+	return new RestTemplate();
+    }
+
     
 
 }
